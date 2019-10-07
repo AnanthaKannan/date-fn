82e299cde81359322aa37b60630d5f874f4b3377
AnanthaKannan <sreeananthakannan@gmail.com>
2019-10-08 00:17:15 +0530

    date add subtraction added

---
+++ function/calc.js
@@ -0,0 +1,8 @@
+let from_ = new Date();
+    let to_ = new Date('2018-08-28')
+
+sub(from_, to_);
+function sub(from, to){
+    
+    console.log(from_.getTime())
+}
--- index.js
+++ index.js
@@ -9,3 +9,30 @@ const { date } = require('./router');
 module.exports = {
     date
 }
+
+
+
+
+
+
+
+let from_ = new Date('2018-08-29');
+    let to_ = new Date('2018-08-28')
+
+// sub(from_, to_);
+function sub(from, to){
+    const milisec = from.getTime() - to.getTime();
+    const seconds  = (milisec / 1000).toFixed(1);
+    const minutes  = (milisec / (1000 * 60)).toFixed(1);
+    const hours  = (milisec / (1000 * 60 * 60) ).toFixed(1);
+    const days  = (milisec / (1000 * 60 * 60 * 24)).toFixed(1);
+    let ret = { seconds, minutes, hours, days };
+    console.log(ret);
+}
+
+add(from_, 3);
+function add(date, no){
+    const d =  new Date(date.setDate( date.getDate() - no ));
+    console.log(d);
+}
+
